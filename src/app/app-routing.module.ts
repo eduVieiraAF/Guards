27e9b744658da944797380afe250f2fb2225035f
eduardo.Vieira.AF <eduardo.vieira@erpserv.con.br>
2023-10-20 22:38:32 -0300

    Route guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
+import { AccountComponent } from './shared/account/account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
